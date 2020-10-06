017e4df7dcb0573196a6bedec134239a3e84726c
Kyle Illana <kyleillana@gmail.com>
2020-10-06 17:16:01 -0400

    part 2 push

---
+++ Poly Defense/Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    void Start()
+    {
+        target = Waypoints.waypoints[0];
+    }
+
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.8f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        if (wavepointIndex >= Waypoints.waypoints.Length - 1)
+        {
+            Destroy(gameObject);
+        }
+        wavepointIndex++;
+        target = Waypoints.waypoints[wavepointIndex];
+    }
+}
