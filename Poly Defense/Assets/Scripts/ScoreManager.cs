152925ba12b6eb488d5808577fe0b8bcaafee116
chasenater23 <chase.johnson@hotmail.com>
2020-10-07 18:31:32 -0400

    Main Menu and Scoreboard Update
    Worked a bit on basic menu and added scoreboard stuff

---
+++ Poly Defense/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+
+    public int score, highScore;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddScore()
+    {
+        score++;
+    }
+}
