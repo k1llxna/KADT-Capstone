a442ac78a2a9fbb0b77521454e9c0628a9c2f68d
Kyle Illana <kyleillana@gmail.com>
2020-10-07 16:58:53 -0400

    Enemy Steering

--- Poly Defense/Assets/Scripts/Waypoints.cs
+++ Poly Defense/Assets/Scripts/Waypoints.cs
@@ -3,7 +3,6 @@
 public class Waypoints : MonoBehaviour
 {
     public static Transform[] waypoints;
-
     // find child of parented waypoints
     void Awake()
     {
@@ -13,5 +12,24 @@ public class Waypoints : MonoBehaviour
         }
     }
 
+    //void DrawCircle(this GameObject container, float radius, float lineWidth)
+    //{
+    //    var segments = 360;
+    //    var line = container.AddComponent<LineRenderer>();
+    //    line.useWorldSpace = false;
+    //    line.startWidth = lineWidth;
+    //    line.endWidth = lineWidth;
+    //    line.positionCount = segments + 1;
 
+    //    var pointCount = segments + 1; // add extra point to make startpoint and endpoint the same to close the circle
+    //    var points = new Vector3[pointCount];
+
+    //    for (int i = 0; i < pointCount; i++)
+    //    {
+    //        var rad = Mathf.Deg2Rad * (i * 360f / segments);
+    //        points[i] = new Vector3(Mathf.Sin(rad) * radius, 0, Mathf.Cos(rad) * radius);
+    //    }
+
+    //    line.SetPositions(points);
+    //}
 }
