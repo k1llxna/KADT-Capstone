19ca9af50c85e692d75f05c989be671daaaa4cf5
Kyle Illana <kyleillana@gmail.com>
2020-10-14 23:48:26 -0400

    turret & turret lock on

--- Poly Defense/Assets/Scripts/Tower/Structure.cs
+++ Poly Defense/Assets/Scripts/Tower/Structure.cs
@@ -1,30 +1,79 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Structure : MonoBehaviour
 {
-
     public float health;
 
+    private Transform target;
 
+    public float range = 15f;
+    public string enemyTag = "Enemy";
+
+    public Transform rotator;
+    public float turnSpeed = 10f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // per x sec
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        // store closest enemy found
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        } else
+        {
+            target = null;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(health <= 0)
-        {
-            Destroy(gameObject);
-        }
+
+        if (target == null)
+            return;
+
+        // look at target
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(rotator.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles; // XYZ
+        rotator.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+        //if(health <= 0)
+        //{
+        //    Destroy(gameObject);
+        //}
     }
 
     public void DealDamage(float damage)
     {
         health -= damage;
+
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
     }
 }
+
