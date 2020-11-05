b8c4b20e277055b346fb2b6a33913a6dc65d6bd7
KeirnanO <45085114+KeirnanO@users.noreply.github.com>
2020-11-04 20:37:41 -0500

    Ai
    Graph and CameFrom Pathfinding Method - Uses BFS

---
+++ Poly Defense/Assets/Scripts/Ai/PathFinding/Navigation/BFS.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GraphSearch<T>
+{
+    public static Dictionary<Node<T>, Node<T>> Search(Graph<T> graph, Node<T> start)
+    {
+        var frontier = new Queue<Node<T>>();
+        frontier.Enqueue(start);
+
+        var cameFrom = new Dictionary<Node<T>, Node<T>>();
+        
+        cameFrom[start] = default(Node<T>);
+
+        while (frontier.Count > 0)
+        {
+            Node<T> current = frontier.Dequeue();
+
+            foreach (var next in graph.Neighbours(current))
+            {
+                if (!cameFrom.ContainsKey(next))
+                {
+                    frontier.Enqueue(next);
+                    cameFrom.Add(next, current);
+                }
+            }
+        }
+
+        return cameFrom;
+    }
+}
