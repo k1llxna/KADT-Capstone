8dc8775f8cba73bee332100aa09d738563874b54
chasenater23 <chase.johnson@hotmail.com>
2020-12-15 21:03:16 -0500

    Chat Room Manager

---
+++ Poly Defense/Assets/ChatRoomButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChatRoomButton : MonoBehaviour
+{
+
+    public void ToggleVisability()
+    {
+        Renderer rend = gameObject.GetComponent<Renderer>();
+
+        if (rend.enabled)
+            rend.enabled = false;
+        else
+            rend.enabled = true;
+    }
+}
